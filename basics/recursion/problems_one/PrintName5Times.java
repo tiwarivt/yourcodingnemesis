5a3a74a9262b448b87c1e7474d5f097bdc03bf77
VT <>
2024-03-28 21:34:30 +0530

    q1

---
+++ basics/recursion/problems_one/PrintName5Times.java
@@ -0,0 +1,41 @@
+/**
+ * PrintName5Times
+ */
+
+ //using static 
+public class PrintName5Times {
+
+    static int i = 5;
+    private static void printName5Times() {
+        if (i != 0) {
+            System.out.println("VT");
+            i--;
+            printName5Times();
+        }
+        return;
+    }
+
+    public static void main(String[] args) {
+        printName5Times();
+    }
+}
+
+
+//without using static
+/*
+public class PrintName5Times {
+
+    private static void printName5Times(int i) {
+        if (i != 0) {
+            System.out.println("VT");
+            printName5Times(i-1);
+            i--;
+        }
+        return;
+    }
+
+    public static void main(String[] args) {
+        printName5Times(5);
+    }
+}
+*/
