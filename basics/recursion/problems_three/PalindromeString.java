969eda216a2a1ea4f31da6adf1ace0874f2126ee
VT <writetovt7@gmail.com>
2024-03-30 00:05:50 +0530

    pali

--- basics/recursion/problems_three/PalindromeString.java
+++ basics/recursion/problems_three/PalindromeString.java
@@ -1,23 +1,28 @@
 package basics.recursion.problems_three;
 
 public class PalindromeString {
+    private static boolean flag = false;
 
-    static boolean pali(StringBuilder temp, String str) {
-        int len = str.length() - 2;
-        if(len > 0){
-            temp.append(temp.charAt(len));
-            len--;
+    private static boolean pali(StringBuilder temp, String str, int len) {
+        if (len >= 0) {
+            temp.append(str.charAt(len));
+        } else {
+            return false;
         }
-        if(str.equals(temp.toString())){
-            return true;
+        if (str.equals(temp.toString())) {
+            flag = true;
+        } else {
+            flag = false;
         }
-        return pali(temp, str);
+        pali(temp, str, len - 1);
+        return flag;
     }
 
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
-        String str = "palindrome";
-        // String str = "madam";
-        System.out.println(pali(sb, str));
+        // String str = "palindrome";
+        String str = "madam";
+        int len = str.length() - 1;
+        System.out.println(pali(sb, str, len));
     }
 }
