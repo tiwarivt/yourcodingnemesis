02753c51a2cbf9e3c53940d178da9461c5ae331d
VT <writetovt7@gmail.com>
2024-03-29 16:31:29 +0530

    rev array

---
+++ basics/recursion/problems_three/ReverseArray.java
@@ -0,0 +1,13 @@
+package basics.recursion.problems_three;
+
+public class ReverseArray {
+
+    public static int[] revArray(int[] arr) {
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 9};
+        System.out.println(revArray(arr));
+    }
+}
