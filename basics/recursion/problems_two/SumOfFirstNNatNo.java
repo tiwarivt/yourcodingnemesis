82d65037c3df98e25ccae4ffe34ca242d9cf0d4a
VT <>
2024-03-29 12:45:23 +0530

    two

--- basics/recursion/problems_two/SumOfFirstNNatNo.java
+++ basics/recursion/problems_two/SumOfFirstNNatNo.java
@@ -1,17 +1,18 @@
 package basics.recursion.problems_two;
 
 public class SumOfFirstNNatNo {
+    static int s = 1;
 
-    private static int sum(int s, int n) {
-        if (n == 0) {
-            return n;
+    private static int sum(int n) {
+        if (n == 1) {
+            return s;
         }
-        s += sum(s, n - 1);
+        s = sum(n) + sum(n - 1);
         return s;
     }
 
     public static void main(String[] args) {
         int n = 10;
-        sum(0, n);
+        System.out.println(sum(n));
     }
 }
